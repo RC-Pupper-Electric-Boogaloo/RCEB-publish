fe02fe3ba210839e203286f82db19ff9c2bd4d96
Majuniemi <149418950+Majuniemi@users.noreply.github.com>
2024-11-06 17:08:37 +0200

    Obstacle komponentti tehty ja lisätty fysiikkamoottoriin

--- entities/index.js
+++ entities/index.js
@@ -1,18 +1,20 @@
 import Matter from "matter-js"
 
 import { Dimensions } from "react-native";
+import Obstacle from "../components/Obstacle";
 
 const windowHeight = Dimensions.get('window').height
 const windowWidth = Dimensions.get('window').width
 
 export default restart => {
-    let engine = Matter.Engine.create({enableSleeping: false})
+    let engine = Matter.Engine.create({ enableSleeping: false })
 
     let world = engine.world
 
     world.gravity.y = 0.4;
 
-    return{
-        physics: {engine, world},
+    return {
+        physics: { engine, world },
+        Obstacle: Obstacle(world, 'black', { x: 60, y: 200 }, { height: 100, width: 100 }),
     }
 }
