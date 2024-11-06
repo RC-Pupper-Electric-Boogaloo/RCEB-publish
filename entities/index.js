1e8fe371eb81e37c96309fa00e83d8fb8c426afc
xolooh <tapsukk@hotmail.fi>
2024-11-06 17:24:39 +0200

    RCEB #10

--- entities/index.js
+++ entities/index.js
@@ -1,18 +1,22 @@
 import Matter from "matter-js"
-
+import Char from "../Components/Char"
 import { Dimensions } from "react-native";
 
 const windowHeight = Dimensions.get('window').height
 const windowWidth = Dimensions.get('window').width
+
+const initialCharPositionX = windowWidth / 2;
+const initialCharPositionY = windowHeight - 30
 
 export default restart => {
     let engine = Matter.Engine.create({enableSleeping: false})
 
     let world = engine.world
 
-    world.gravity.y = 0.4;
+    world.gravity.y = 0 // muutettu 0.4-->0 jottei char komponentti tipu ruudula
 
     return{
         physics: {engine, world},
+        Char: Char(world, 'red', { x: initialCharPositionX, y: initialCharPositionY }, { height: 40, width: 40 })
     }
 }
