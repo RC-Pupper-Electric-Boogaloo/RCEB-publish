a7b8a1b8b8e65152ea2a8111fc8defdc10b5493a
Majuniemi <149418950+Majuniemi@users.noreply.github.com>
2024-11-13 04:00:42 +0200

    Refactor GameScreen and HighscoreScreen components for improved navigation and code clarity

--- screens/gameScreen.js
+++ screens/gameScreen.js
@@ -5,15 +5,13 @@ import { StatusBar } from 'expo-status-bar';
 import entities from '../entities';
 import Physics from '../physics';
 import BackgroundMusic, { usePlayCollisionSound, usePlayPointSound } from '../components/BackgroundMusic';
-import HighscoreScreen from '../screens/highscoreScreen';
 
-const GameScreen = () => {
+export default function GameScreen({ navigation }) {
     const [running, setRunning] = useState(false);
     const [gameEngine, setGameEngine] = useState(null);
     const [currentPoints, setCurrentPoints] = useState(0);
     const playCollisionSound = usePlayCollisionSound();
     const playPointSound = usePlayPointSound();
-    const [showHighscores, setShowHighscores] = useState(false);
     const stopMusicRef = useRef(); // Tämä on tärkeää, jotta stopRef on alustettu
 
     useEffect(() => {
@@ -21,75 +19,67 @@ const GameScreen = () => {
     }, []);
 
     const handleShowHighScores = () => {
-        setShowHighscores(true);
+        navigation.navigate('Highscore', { points: currentPoints })
     };
 
     return (
         <View style={{ flex: 1, backgroundColor: '#b4b4b4' }}>
-            {showHighscores ? (
-                <HighscoreScreen points={currentPoints} onReturn={() => setShowHighscores(false)} />
-            ) : (
-                <>
-                    <Text style={{ textAlign: 'center', fontSize: 40, fontWeight: 'bold', margin: 20, zIndex: 100, position: 'absolute', right: 20, }}>
-                        {currentPoints}
-                    </Text>
+            <Text style={{ textAlign: 'center', fontSize: 40, fontWeight: 'bold', margin: 20, zIndex: 100, position: 'absolute', right: 20, }}>
+                {currentPoints}
+            </Text>
 
-                    <BackgroundMusic stopRef={stopMusicRef} />
-                    <GameEngine
-                        ref={(ref) => { setGameEngine(ref); }}
-                        systems={[Physics]}
-                        entities={entities()}
-                        running={running}
-                        onEvent={(e) => {
-                            switch (e.type) {
-                                case 'game_over':
-                                    playCollisionSound();
-                                    if (stopMusicRef.current) {
-                                        stopMusicRef.current(); // Pysäytä musiikki
-                                    }
-                                    setRunning(false);
-                                    gameEngine.stop();
-                                    handleShowHighScores();
-                                    break;
-                                case 'new_point':
-                                    playPointSound();
-                                    setCurrentPoints(currentPoints + 1);
-                                    break;
-                                case 'miss':
-                                    playCollisionSound();
-                                    if (currentPoints >= 5) {
-                                        setCurrentPoints(currentPoints - 5);
-                                    } else {
-                                        setCurrentPoints(0);
-                                    }
-                                    break;
+            <BackgroundMusic stopRef={stopMusicRef} />
+            <GameEngine
+                ref={(ref) => { setGameEngine(ref); }}
+                systems={[Physics]}
+                entities={entities()}
+                running={running}
+                onEvent={(e) => {
+                    switch (e.type) {
+                        case 'game_over':
+                            playCollisionSound();
+                            if (stopMusicRef.current) {
+                                stopMusicRef.current(); // Pysäytä musiikki
                             }
+                            setRunning(false);
+                            gameEngine.stop();
+                            handleShowHighScores();
+                            break;
+                        case 'new_point':
+                            playPointSound();
+                            setCurrentPoints(currentPoints + 1);
+                            break;
+                        case 'miss':
+                            playCollisionSound();
+                            if (currentPoints >= 5) {
+                                setCurrentPoints(currentPoints - 5);
+                            } else {
+                                setCurrentPoints(0);
+                            }
+                            break;
+                    }
+                }}
+                style={{ position: 'absolute', top: 0, left: 0, right: 0, bottom: 0 }}
+            >
+                <StatusBar style="auto" hidden={true} />
+            </GameEngine>
+            {!running ? (
+                <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+                    <TouchableOpacity
+                        style={{ backgroundColor: 'black', paddingHorizontal: 30, paddingVertical: 10 }}
+                        onPress={() => {
+                            setCurrentPoints(0);
+                            setRunning(true);
+                            gameEngine.swap(entities());
+                            gameEngine.start();
                         }}
-                        style={{ position: 'absolute', top: 0, left: 0, right: 0, bottom: 0 }}
                     >
-                        <StatusBar style="auto" hidden={true} />
-                    </GameEngine>
-                    {!running ? (
-                        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-                            <TouchableOpacity
-                                style={{ backgroundColor: 'black', paddingHorizontal: 30, paddingVertical: 10 }}
-                                onPress={() => {
-                                    setCurrentPoints(0);
-                                    setRunning(true);
-                                    gameEngine.swap(entities());
-                                    gameEngine.start();
-                                }}
-                            >
-                                <Text style={{ fontWeight: 'bold', color: 'white', fontSize: 30 }}>
-                                    START GAME
-                                </Text>
-                            </TouchableOpacity>
-                        </View>
-                    ) : null}
-                </>
-            )}
-        </View>
+                        <Text style={{ fontWeight: 'bold', color: 'white', fontSize: 30 }}>
+                            START GAME
+                        </Text>
+                    </TouchableOpacity>
+                </View>
+            ) : null}
+        </View >
     );
 }
-
-export default GameScreen;
--- screens/highscoreScreen.js
+++ screens/highscoreScreen.js
@@ -1,44 +1,44 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, FlatList, StyleSheet, Button, TextInput } from 'react-native';
+import { View, Text, FlatList, StyleSheet, TouchableOpacity, TextInput } from 'react-native';
 import Constants from 'expo-constants';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const HighscoreScreen = ({points, onReturn}) => {
+export default function HighscoreScreen({ points, onReturn, navigation }) {
     const [highScores, setHighScores] = useState([]);
-    
- // Lataa ja päivittää highscoret aluksi ja aina, kun points muuttuu
- useEffect(() => {
-    const saveAndLoadScores = async () => {
-      if (points) await savePoints(points);
-      const scores = await loadHighScores();
-      setHighScores(scores);
-    };
-    saveAndLoadScores();
-  }, [points]);
 
-// Tallentaa pisteet AsyncStorageen
-const savePoints = async (points) => {
-    try {
-        const savedScores = await AsyncStorage.getItem('HIGHSCORES'); // Ladataan nykyiset highscoret AsyncStorageista
-        let scoresArray = savedScores ? JSON.parse(savedScores) : [];
-        scoresArray.push(points); // Lisää nykyiset pisteet listaan
-        await AsyncStorage.setItem('HIGHSCORES', JSON.stringify(scoresArray));  // Tallennetaan päivitetty lista takaisin AsyncStorageiin
-    } catch (e) {
-        console.error("Pisteiden tallennus epäonnistui", e);
-    }
-};
-  // Ladataan highscoret AsyncStoragesta
-const loadHighScores = async () => {
-    try {
-        const savedScores = await AsyncStorage.getItem('HIGHSCORES');// Haetaan tallennetut highscoret
-        let scoresArray = savedScores ? JSON.parse(savedScores) : [];
-          // Järjestetään tulokset laskevaan järjestykseen ja pidetään vain top 10
-          return scoresArray.sort((a, b) => b - a).slice(0, 10);
-    } catch (e) {
-        console.error("Highscorejen lataaminen epäonnistui", e);
-        return [];
-    }
-};
+    // Lataa ja päivittää highscoret aluksi ja aina, kun points muuttuu
+    useEffect(() => {
+        const saveAndLoadScores = async () => {
+            if (points) await savePoints(points);
+            const scores = await loadHighScores();
+            setHighScores(scores);
+        };
+        saveAndLoadScores();
+    }, [points]);
+
+    // Tallentaa pisteet AsyncStorageen
+    const savePoints = async (points) => {
+        try {
+            const savedScores = await AsyncStorage.getItem('HIGHSCORES'); // Ladataan nykyiset highscoret AsyncStorageista
+            let scoresArray = savedScores ? JSON.parse(savedScores) : [];
+            scoresArray.push(points); // Lisää nykyiset pisteet listaan
+            await AsyncStorage.setItem('HIGHSCORES', JSON.stringify(scoresArray));  // Tallennetaan päivitetty lista takaisin AsyncStorageiin
+        } catch (e) {
+            console.error("Pisteiden tallennus epäonnistui", e);
+        }
+    };
+    // Ladataan highscoret AsyncStoragesta
+    const loadHighScores = async () => {
+        try {
+            const savedScores = await AsyncStorage.getItem('HIGHSCORES');// Haetaan tallennetut highscoret
+            let scoresArray = savedScores ? JSON.parse(savedScores) : [];
+            // Järjestetään tulokset laskevaan järjestykseen ja pidetään vain top 10
+            return scoresArray.sort((a, b) => b - a).slice(0, 10);
+        } catch (e) {
+            console.error("Highscorejen lataaminen epäonnistui", e);
+            return [];
+        }
+    };
 
     const renderItem = ({ item, index }) => (
         <View style={styles.item}>
@@ -56,7 +56,13 @@ const loadHighScores = async () => {
                 renderItem={renderItem}
                 keyExtractor={(item, index) => index.toString()}
             />
-             <Button title="Return" onPress={onReturn} />
+            <TouchableOpacity style={styles.button}
+                onPress={() => { navigation.navigate('MainMenu') }}
+            >
+                <Text style={styles.buttonText}>
+                    RETURN
+                </Text>
+            </TouchableOpacity>
         </View>
     );
 };
@@ -86,15 +92,17 @@ const styles = StyleSheet.create({
         borderBottomColor: '#ccc',
     },
     button: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        width: '100%',
-        padding: 20,
-        marginBottom: 20,
+        backgroundColor: 'black',
+        paddingHorizontal: 30,
+        paddingVertical: 10,
+        width: '70%',
+        marginBottom: 20
     },
-    returnbutton: {
-        marginTop: 20,
-        width: '60%',
+    buttonText: {
+        fontWeight: 'bold',
+        color: 'white',
+        fontSize: 30,
+        textAlign: 'center'
     },
     name: {
         fontSize: 18,
@@ -102,6 +110,4 @@ const styles = StyleSheet.create({
     score: {
         fontSize: 18,
     },
-});
-
-export default HighscoreScreen;
+})
