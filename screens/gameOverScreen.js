53f6cca4c7f11dc8689569c87d6fbe11e29f0a86
Majuniemi <149418950+Majuniemi@users.noreply.github.com>
2024-12-02 18:24:19 +0200

    Updated button styles on Game Over screen for consistency

--- screens/gameOverScreen.js
+++ screens/gameOverScreen.js
@@ -82,8 +82,8 @@ const GameOverScreen = ({ currentPoints, coinCount, onRestart, onShowHighscores,
                             placeholderTextColor={'#FD8A0B'}
                             autoCapitalize='characters'
                         />
-                        <TouchableOpacity style={styles.buttonGameover} onPress={saveHighScore}>
-                            <Text style={styles.buttonTextGameOver}>Save Score</Text>
+                        <TouchableOpacity style={styles.button} onPress={saveHighScore}>
+                            <Text style={styles.title}>Save Score</Text>
                         </TouchableOpacity>
                     </View>
                 ) : (
